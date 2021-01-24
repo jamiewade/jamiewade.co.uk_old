e879cbd74efd91917fc678b93ed096b0c282d087
Jamie Wade <jamiewade94@icloud.com>
2021-01-24 15:08:24 +0000

    Adds Google Tag Manager

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,14 @@
 module.exports = {
-  siteMetadata: {
-    title: "Jamie Wade",
-  },
-  plugins: ['gatsby-plugin-postcss'],
+    siteMetadata: {
+        title: "Jamie Wade",
+    },
+    plugins: [
+        'gatsby-plugin-postcss',
+        {
+            resolve: 'gatsby-plugin-google-tagmanager',
+            options: {
+                id: 'GTM-T94WKP7'
+            }
+        }
+    ],
 };
