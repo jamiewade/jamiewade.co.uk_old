df6960551eff8241cdfa0ffac9217c8564df2d2f
Jamie Wade <jamiewade94@icloud.com>
2021-01-24 14:43:41 +0000

    Adjusts the homepage content elements spacing and sizing

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,12 +16,12 @@ export default function Home() {
 
                         <h1 className={"mb-3 font-bold text-gray-800 text-3xl sm:mb-4 sm:text-4xl md:text-5xl"}>Nice to meet you <span role={"img"} aria-label={"Waving hand emoji"}>👋</span></h1>
 
-                        <p className={"mb-1 sm:mb-2 sm:text-lg md:text-xl"}>I'm Jamie Wade. I'm a front-end developer based in the UK, working with Craft CMS, Tailwind, and Gatsby.</p>
+                        <p className={"mb-2 sm:mb-3 sm:text-lg md:text-xl"}>I'm Jamie Wade. I'm a front-end developer based in the UK, working with Craft CMS, Tailwind, and Gatsby.</p>
 
-                        <p className={"mb-6 text-gray-400 sm:mb-8 md:mb-10 md:text-lg"}>I also have experience with Shopify, Vue, and Laravel.</p>
+                        <p className={"mb-8 text-gray-400 sm:mb-10 md:mb-10 md:text-lg"}>I also have experience with Shopify, Vue, and Laravel.</p>
 
                         <p>
-                            <a href="mailto:jamie.wade.contact@icloud.com" className={"rounded bg-indigo-600 p-2.5 text-white md:p-3 md:text-lg duration-300 hover:bg-indigo-700"}>Get in touch</a>
+                            <a href="mailto:jamie.wade.contact@icloud.com" className={"rounded bg-indigo-600 p-2.5 text-white sm:p-3 sm:text-lg md:p-3.5 md:text-xl duration-300 hover:bg-indigo-700"}>Get in touch</a>
                         </p>
                     </div>
                 </Content>
