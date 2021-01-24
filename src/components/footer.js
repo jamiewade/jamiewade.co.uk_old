20be7998b2cf230105fe0bc0fedcde8428e613dc
Jamie Wade <jamiewade94@icloud.com>
2021-01-24 14:32:12 +0000

    Footer front-end adjustments

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,18 +5,18 @@ const today = new Date();
 
 export default function Footer() {
     return (
-        <footer className={"text-gray-400 text-sm"}>
-            <div className={"flex flex-col py-6 text-center container sm:py-8"}>
-                <div>
-                    <p>Built with &hearts; using Gatsby & Tailwind</p>
-                </div>
+        <footer className={"text-gray-400 text-center text-sm"}>
+            <div className={"container"}>
+                <div className={"grid gap-3 border-t border-gray-100 py-6 md:flex md:justify-between"}>
+                    <div className={"md:order-2"}>
+                        <p>Built with &hearts; using Gatsby & Tailwind</p>
+                    </div>
 
-                <div className={"flex flex-col mt-4 border-t border-gray-100 pt-4 sm:flex-row sm:items-center sm:justify-between sm:mt-6 sm:pt-6"}>
-                    <div>
+                    <div className={"md:order-1"}>
                         <p>&copy; {today.getFullYear()} Jamie Wade. All rights reserved</p>
                     </div>
 
-                    <div className={"mt-3 sm:mt-0"}>
+                    <div className={"md:order-3"}>
                         <img src={gitHub}
                              alt={"Follow me on GitHub"}
                              className={"block mx-auto w-6"}
