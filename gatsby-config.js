e03cb54a1bb5b9c40166eb3e17ab626aed93ca72
Jamie Wade <jamiewade94@icloud.com>
2021-01-24 15:28:59 +0000

    Adds manifest and favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,18 @@ module.exports = {
         title: "Jamie Wade",
     },
     plugins: [
+        {
+            resolve: 'gatsby-plugin-manifest',
+            options: {
+                name: 'Jamie Wade',
+                short_name: 'jamiewade',
+                start_url: '/',
+                background_color: '#fff',
+                theme_color: 'rgba(79, 70, 229)',
+                display: 'minimal-ui',
+                icon: 'src/images/icon.png'
+            },
+        },
         'gatsby-plugin-postcss',
         {
             resolve: 'gatsby-plugin-google-tagmanager',
