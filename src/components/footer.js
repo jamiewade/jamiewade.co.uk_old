917a097c2df6a2aefbb23510328c5307eb5224be
Jamie Wade <jamiewade94@icloud.com>
2021-02-07 16:56:40 +0000

    Homepage animations markup

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,7 +5,12 @@ const today = new Date();
 
 export default function Footer() {
     return (
-        <footer className={"pt-8 text-gray-400 text-center text-sm"}>
+        <footer className={"pt-8 text-gray-400 text-center text-sm"}
+                data-aos={"fade-zoom-in"}
+                data-aos-easing={"ease-in-back"}
+                data-aos-delay={"3000"}
+                data-aos-duration={"750"}
+                data-aos-offset={"0"}>
             <div className={"container"}>
                 <div className={"grid gap-3 border-t border-gray-100 py-6 md:flex md:justify-between"}>
                     <div className={"md:order-2"}>
--- src/pages/index.js
+++ src/pages/index.js
@@ -25,15 +25,39 @@ class IndexPage extends React.Component {
                             <img src={memoji}
                                  alt={"Jamie Wade"}
                                  className={"block mx-auto mb-6 w-36"}
+                                 data-aos={"fade-zoom-in"}
+                                 data-aos-easing={"ease-in-back"}
+                                 data-aos-delay={"2500"}
+                                 data-aos-duration={"750"}
+                                 data-aos-offset={"0"}
                                  loading={"lazy"}/>
 
-                            <h1 className={"mb-3 font-bold text-gray-800 text-3xl sm:mb-4 sm:text-4xl md:text-5xl"}>Nice to meet you <span role={"img"} aria-label={"Waving hand emoji"}>👋</span></h1>
+                            <h1 className={"mb-3 font-bold text-gray-800 text-3xl sm:mb-4 sm:text-4xl md:text-5xl"}
+                                data-aos={"fade-zoom-in"}
+                                data-aos-easing={"ease-in-back"}
+                                data-aos-delay={"250"}
+                                data-aos-duration={"750"}
+                                data-aos-offset={"0"}>Nice to meet you <span role={"img"} aria-label={"Waving hand emoji"}>👋</span></h1>
 
-                            <p className={"mb-2 sm:mb-3 sm:text-lg md:text-xl"}>I'm Jamie Wade. I'm a Front-end Developer based in the UK, working with Craft CMS, Tailwind, Gatsby, and Alpine.js.</p>
+                            <p className={"mb-2 sm:mb-3 sm:text-lg md:text-xl"}
+                               data-aos={"fade-zoom-in"}
+                               data-aos-easing={"ease-in-back"}
+                               data-aos-delay={"1000"}
+                               data-aos-duration={"750"}
+                               data-aos-offset={"0"}>I'm Jamie Wade. I'm a Front-end Developer based in the UK, working with Craft CMS, Tailwind, Gatsby, and Alpine.js.</p>
 
-                            <p className={"mb-8 text-gray-400 sm:mb-10 md:mb-10 md:text-lg"}>I also have experience with Shopify, Vue, and Laravel.</p>
+                            <p className={"mb-8 text-gray-400 sm:mb-10 md:mb-10 md:text-lg"}
+                               data-aos={"fade-zoom-in"}
+                               data-aos-easing={"ease-in-back"}
+                               data-aos-delay={"1500"}
+                               data-aos-duration={"750"}
+                               data-aos-offset={"0"}>I also have experience with Shopify, Vue, and Laravel.</p>
 
-                            <p>
+                            <p data-aos={"fade-zoom-in"}
+                               data-aos-easing={"ease-in-back"}
+                               data-aos-delay={"2000"}
+                               data-aos-duration={"750"}
+                               data-aos-offset={"0"}>
                                 <a href="mailto:jamie.wade.contact@icloud.com" className={"rounded bg-indigo-600 p-2.5 text-white sm:p-3 sm:text-lg md:p-3.5 md:text-xl duration-300 hover:bg-indigo-700"}>Get in touch</a>
                             </p>
                         </div>
